bc55659bbf5a006905348484a945560cd3a4ef4c
Elysia Nason <elysia.avery@gmail.com>
2016-09-16 08:10:28 -0700

    add passing tests for Definition object

---
+++ src/main/java/Definition.java
@@ -0,0 +1,35 @@
+import java.util.List;
+import java.util.ArrayList;
+
+public class Definition {
+  private String mUserDefinition;
+  private static List<Definition> definitions = new ArrayList<Definition>();
+  private int mId;
+
+  public Definition(String userDefinition) {
+    mUserDefinition = userDefinition;
+    definitions.add(this);
+    mId = definitions.size();
+  }
+
+  public String getUserDefinition() {
+    return mUserDefinition;
+  }
+
+  public static List<Definition> all() {
+    return definitions;
+  }
+
+  public static void clear() {
+    definitions.clear();
+  }
+
+  public int getId() {
+    return mId;
+  }
+
+  public static Definition find(int id) {
+    return definitions.get(id - 1);
+  }
+
+}
--- src/test/java/DefinitionTest.java
+++ src/test/java/DefinitionTest.java
@@ -1,2 +1,45 @@
 import org.junit.*;
 import static org.junit.Assert.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DefinitionTest {
+
+  @After
+  public void tearDown() {
+    Definition.clear();
+  }
+
+  @Test
+  public void definition_instantiatesCorrectly_true() {
+    Definition testDefinition = new Definition("a mammal...");
+    assertTrue(testDefinition instanceof Definition);
+  }
+
+  @Test
+  public void getUserDefinition_definitionInstantiatesWithUserDefinition_Dog() {
+    Definition testDefinition = new Definition("a mammal...");
+    assertEquals("a mammal...", testDefinition.getUserDefinition());
+  }
+
+  @Test
+  public void all_returnsAllInstancesOfDefinition_true() {
+    Definition firstDefinition = new Definition("a mammal...");
+    Definition secondDefinition = new Definition("a domesticated mammal...");
+    assertTrue(Definition.all().contains(firstDefinition));
+    assertTrue(Definition.all().contains(secondDefinition));
+  }
+
+  @Test
+  public void getId_definitionsInstantiateWithAnId_1() {
+    Definition testDefinition = new Definition("a mammal...");
+    assertEquals(1, testDefinition.getId());
+  }
+
+  @Test
+  public void find_returnsDefinitionWithSAmeId_SecondDefinition() {
+    Definition firstDefinition = new Definition("a mammal...");
+    Definition secondDefinition = new Definition("a domesticated mammal...");
+    assertEquals(Definition.find(secondDefinition.getId()), secondDefinition);
+  }
+}
